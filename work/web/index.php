9bb8934dd86b266f7bcf393549dc0ea623ec2a63
kd0171 <kido.0117.1895@gmail.com>
2023-12-23 17:01:48 +0100

    別ファイルに関数をまとめる

--- work/web/index.php
+++ work/web/index.php
@@ -1,4 +1,10 @@
 <?php
+require('../app/functions.php');
+// // html中に長文を書くのは憚られるので、こちらで定義
+// function h($str)
+// {
+//   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+// }
 
 // $name = 'Taro';
 $name = 'Taro <script>alert(1);</script>';
@@ -16,6 +22,6 @@ $name = 'Taro <script>alert(1);</script>';
   <!-- <p>Hello, <?= $name; ?>: PHP!</p> -->
   <!-- htmlspecialchars(変換したい文字列, ENT_QUOTES, WEBページの文字コード) -->
   <!-- HTML に値を埋め込むときには、必ずこうした処理をする -->
-  <p>Hello, <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8'); ?>!</p>
+  <p>Hello, <?= h($name); ?>!</p>
 </body>
 </html>
