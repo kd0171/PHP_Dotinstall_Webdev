06afb7d994acf06ef5a8ccd9def07cf3980b85d5
kd0171 <kido.0117.1895@gmail.com>
2023-12-23 16:51:05 +0100

    PHPとhtml、変数の利用

--- work/web/index.php
+++ work/web/index.php
@@ -1,3 +1,10 @@
+<?php
+
+$today = date('Y-m-d H:i:s l');
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -7,8 +14,9 @@
 <body>
   <p>Hello, PHP!</p>
   <p>Today: <?php echo date('Y-m-d H:i:s l'); ?></p>
-  
+  <!-- 開始タグのphpは省略可能 -->
   <p>Today: <?= date('Y-m-d H:i:s l'); ?></p>
+  <!-- 冒頭で変数を定義することも可能 -->
   <p>Today: <?= $today; ?></p>
 </body>
 </html>
