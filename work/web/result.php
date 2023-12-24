5e895ff43b140ae45062916b0ad2af556ad90ed0
kd0171 <kido.0117.1895@gmail.com>
2023-12-24 03:11:08 +0100

    フォームの送信 送られてきた値を用いるにはfilter_inputを用いる GET形式ではURLを介して値を渡す

--- work/web/result.php
+++ work/web/result.php
@@ -2,19 +2,18 @@
 // 確実に読み込まなければならないファイルにはrequireを、そうでないファイルにはincludeを使う
 require('../app/functions.php');
 
+// 送られてきた値を用いるにはfilter_inputを用いる。形式は現在はGET
+$message = filter_input(INPUT_GET, 'message');
+$username = filter_input(INPUT_GET, 'username');
+
+
 // 読み込みに失敗しても処理を止めるほどクリティカルではない
 include('../app/_parts/_header.php');
 
 ?>
 
-<!-- methodはgetかpostで指定 -->
-  <!-- GET形式ではURLを介して値を渡す -->
-<form action="result.php" method="get">
-<!-- result.phpで送られた値をどの名前で受け取るかをname属性で指定 -->
-  <input type="text" name="message">
-  <input type="text" name="username">
-  <button>Send</button>
-</form>
+  <p><?= h($message); ?> by <?= h($username); ?></p>
+  <p><a href="index.php">Go back</a></p>
 
 
 <?php
