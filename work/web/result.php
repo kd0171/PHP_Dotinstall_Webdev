397745d6762607475bea419bcdb6eb4e61c166ce
kd0171 <kido.0117.1895@gmail.com>
2023-12-24 14:34:57 +0100

    POST機能の実装 if ($_SERVER['REQUEST_METHOD'] === 'POST') は、リクエストがPOSTメソッドである場合に真

--- work/web/result.php
+++ work/web/result.php
@@ -1,11 +1,25 @@
 <?php
 
 require('../app/functions.php');
+// 直接アクセスすると空白ポストが追加されるのでPOSTでのアクセスのみ対応
+//条件文 if ($_SERVER['REQUEST_METHOD'] === 'POST') は、リクエストがPOSTメソッドである場合に真となります。
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // POSTされたデータはfilter_inputで受け取る
+  $message = trim(filter_input(INPUT_POST, 'message'));
+  $message = $message !== '' ? $message : '...';
+
+  $filename = '../app/messages.txt';
+  $fp = fopen($filename, 'a'); //'a'は追記モード
+  fwrite($fp, $message . "\n");
+  fclose($fp);
+} else {
+  exit('Invalid Request');
+}
 
 include('../app/_parts/_header.php');
 
 ?>
-
+  <p>Message added!</p>
   <p><a href="index.php">Go back</a></p>
 
 <?php
